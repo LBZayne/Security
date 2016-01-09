6d1b7ad0e817cff6f7ee6a33377af3bda517a719
吕振 <lvzhenpersonal@163.com>
2016-01-10 04:11:37 +0800

    项目新建

---
+++ Security/Security/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Security
+//
+//  Created by jingbachelor on 16/1/10.
+//  Copyright © 2016年 jingbachelor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
